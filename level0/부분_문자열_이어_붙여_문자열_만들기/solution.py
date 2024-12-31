2ec48218af01245acad2f5f2c5c703b95ffe5f07
kyucchoi <kyucchoi@naver.com>
2024-12-31 11:24:09 +0900

    20241231

---
+++ level0/부분_문자열_이어_붙여_문자열_만들기/solution.py
@@ -0,0 +1,9 @@
+def solution(my_strings, parts):
+   answer = ''
+   
+   # my_strings와 parts를 동시에 순회
+   for string, part in zip(my_strings, parts):
+       s, e = part  # 시작과 끝 인덱스
+       answer += string[s:e+1]  # 부분 문자열 추출하여 이어붙이기
+       
+   return answer
