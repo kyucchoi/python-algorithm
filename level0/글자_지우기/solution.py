af2809b01fc05cae9100329a54bd15ddf535c008
kyucchoi <kyucchoi@naver.com>
2025-01-07 13:03:59 +0900

    20250107

---
+++ level0/글자_지우기/solution.py
@@ -0,0 +1,10 @@
+def solution(my_string, indices):
+   # 문자열을 리스트로 변환
+   string_list = list(my_string)
+   
+   # indices의 위치에 있는 문자들을 빈 문자로 변경
+   for index in indices:
+       string_list[index] = ''
+   
+   # 리스트를 다시 문자열로 합치기
+   return ''.join(string_list)
