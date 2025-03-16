7222c103731e942710591d26cdb249aa31c369dd
kyucchoi <kyucchoi@naver.com>
2025-03-16 16:35:34 +0900

    20250316

---
+++ level1/문자열_내림차순으로_배치하기/solution.py
@@ -0,0 +1,15 @@
+def solution(s):
+    # 문자열을 리스트로 변환
+    chars = list(s)
+    
+    # 내림차순으로 정렬 (reverse=True)
+    chars.sort(reverse=True)
+    
+    # 리스트를 다시 문자열로 변환
+    return ''.join(chars)
+
+# 내 방법
+def solution(s):
+    answer = sorted(s, reverse = True)
+    
+    return ''.join(answer)
