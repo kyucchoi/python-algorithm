022cde92ab53ba1b229acaf08b9cd9f6a5f7fbf2
kyucchoi <kyucchoi@naver.com>
2025-03-23 21:28:37 +0900

    20250323

---
+++ level1/[1차]_비밀지도/solution.py
@@ -0,0 +1,16 @@
+def solution(n, arr1, arr2):
+    result = []
+    
+    for i in range(n):
+        # 두 배열의 해당 위치 값을 이진수로 변환 후 OR 연산
+        binary = bin(arr1[i] | arr2[i])[2:]  # bin() 결과는 '0b'로 시작하므로 [2:]로 제거
+        
+        # n 길이에 맞게 앞에 0 채우기
+        binary = binary.zfill(n)
+        
+        # 1은 '#'으로, 0은 ' '으로 변환
+        row = binary.replace('1', '#').replace('0', ' ')
+        
+        result.append(row)
+    
+    return result
