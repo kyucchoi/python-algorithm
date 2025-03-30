93ae760b9ee2e1c3add94f6868044fd5c914c60e
kyucchoi <kyucchoi@naver.com>
2025-03-30 23:22:07 +0900

    20250330

--- level1/모의고사/solution.py
+++ level1/모의고사/solution.py
@@ -20,6 +20,14 @@ def solution(answers):
     max_score = max(score)
     
     # 가장 높은 점수를 받은 사람들 찾기
-    result = [i + 1 for i in range(len(score)) if score[i] == max_score]
+    # result = [i + 1 for i in range(len(score)) if score[i] == max_score]
+    result = []  # 결과를 저장할 빈 리스트 생성
+
+    # score 리스트의 인덱스를 하나씩 확인
+    for i in range(len(score)):
+        # 현재 인덱스의 점수가 최고 점수와 같은지 확인
+        if score[i] == max_score:
+            # 인덱스는 0부터 시작하지만, 수포자 번호는 1부터 시작하므로 +1
+            result.append(i + 1)
     
     return result
