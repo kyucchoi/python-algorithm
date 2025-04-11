8bbd283d4dc80d0dfc3582dfa21939a06f1d7c28
kyucchoi <kyucchoi@naver.com>
2025-04-12 00:23:17 +0900

    20250412

--- level1/완주하지_못한_선수/solution.py
+++ level1/완주하지_못한_선수/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -7,3 +8,38 @@ def solution(participant, completion):
             return participant[i]
             
     return participant[-1]
+
+# 방법 2
+def solution(participant, completion):
+    # 참가자들의 이름과 등장 횟수를 저장할 딕셔너리
+    participants = {}
+    
+    # 참가자 이름을 딕셔너리에 추가하고 등장 횟수를 카운트
+    for name in participant:
+        if name in participants:
+            participants[name] += 1
+        else:
+            participants[name] = 1
+    
+    # 완주자 이름을 딕셔너리에서 찾아 카운트 감소
+    for name in completion:
+        participants[name] -= 1
+    
+    # 딕셔너리에서 값이 0이 아닌(완주하지 못한) 선수 찾기
+    for name, count in participants.items():
+        if count > 0:
+            return name
+        
+# 방법 3
+from collections import Counter
+
+def solution(participant, completion):
+    # Counter 객체는 요소의 등장 횟수를 세는 딕셔너리처럼 동작
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+    
+    # 두 Counter의 차이는 완주하지 못한 선수를 나타냄
+    result = participant_counter - completion_counter
+    
+    # Counter에서 남은 단 하나의 키(완주하지 못한 선수 이름)를 반환
+    return list(result.keys())[0]
