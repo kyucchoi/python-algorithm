4c3ab30ee4122380715a702b3961e1c0dcf599cb
kyucchoi <kyucchoi@naver.com>
2025-05-02 22:01:07 +0900

    20250502

--- level1/달리기_경주/solution.py
+++ level1/달리기_경주/solution.py
@@ -1,14 +1,11 @@
 def solution(players, callings):
     # 각 선수의 현재 등수를 저장하는 딕셔너리
-    # rank_dict = {player: i for i, player in enumerate(players)}
+    rank_dict = {player: i for i, player in enumerate(players)}
 
-    # 빈 딕셔너리 생성
-    rank_dict = {}
+    # rank_dict = {}
 
-    # enumerate를 사용하여 players 배열을 순회
-    for i, player in enumerate(players):
-        # 각 선수 이름을 키로, 인덱스(등수)를 값으로 딕셔너리에 저장
-        rank_dict[player] = i
+    # for i, player in enumerate(players):
+    #     rank_dict[player] = i
     
     # 각 호출에 대해 등수 변경 처리
     for call in callings:
