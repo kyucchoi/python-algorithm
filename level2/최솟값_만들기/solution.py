cd913910d1bd9eed3268fb0c56a734a145c20450
kyucchoi <kyucchoi@naver.com>
2025-05-12 21:41:57 +0900

    20250512

---
+++ level2/최솟값_만들기/solution.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    A.sort()  # A 배열을 오름차순 정렬
+    B.sort(reverse = True)  # B 배열을 내림차순 정렬
+    
+    total = 0
+    
+    for i in range(len(A)):
+        total += A[i] * B[i]  # 정렬된 두 배열의 같은 인덱스 원소를 곱하여 더함
+    
+    return total
