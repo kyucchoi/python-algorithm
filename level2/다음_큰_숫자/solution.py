a372d7f30e623929cacba6584797aac9563aaed5
kyucchoi <kyucchoi@naver.com>
2025-05-17 11:52:44 +0900

    20250517

--- level2/다음_큰_숫자/solution.py
+++ level2/다음_큰_숫자/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(n):
     # n의 2진수에서 1의 개수
     one_count = bin(n).count('1')
@@ -10,3 +11,17 @@ def solution(n):
         num += 1
         
     return num
+
+# 방법 2
+def solution(n):
+    # n의 2진수에서 1의 개수
+    one_count = bin(n).count('1')
+    
+    # n + 1부터 검사
+    next_num = n + 1
+    
+    # 조건 만족하는 수 찾기
+    while True:
+        if bin(next_num).count('1') == one_count:
+            return next_num
+        next_num += 1
