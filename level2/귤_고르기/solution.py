be26f0fb1f570110bb84344c818ecb72d3902d08
kyucchoi <kyucchoi@naver.com>
2025-05-26 16:22:29 +0900

    20250526

--- level2/귤_고르기/solution.py
+++ level2/귤_고르기/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(k, tangerine):
     # 수동으로 개수 세기
     count_dict = {}
@@ -6,7 +7,7 @@ def solution(k, tangerine):
         count_dict[size] = count_dict.get(size, 0) + 1
     
     # 개수를 기준으로 내림차순 정렬
-    sorted_counts = sorted(count_dict.values(), reverse=True)
+    sorted_counts = sorted(count_dict.values(), reverse = True)
     
     selected = 0
     types = 0
@@ -15,6 +16,29 @@ def solution(k, tangerine):
         selected += cnt
         types += 1
         
+        if selected >= k:
+            break
+    
+    return types
+
+# 방법 2
+def solution(k, tangerine):
+    from collections import Counter
+    
+    # 각 크기별 개수 세기
+    count = Counter(tangerine)
+    
+    # 개수를 기준으로 내림차순 정렬
+    sorted_counts = sorted(count.values(), reverse = True)
+    
+    selected = 0  # 선택한 귤의 개수
+    types = 0     # 사용한 크기 종류의 수
+    
+    for cnt in sorted_counts:
+        selected += cnt
+        types += 1
+        
+        # k개 이상 선택했으면 종료
         if selected >= k:
             break
     
