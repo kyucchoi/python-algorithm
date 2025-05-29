3d4918162fd07a44a115d07b6c367936e69ce11b
kyucchoi <kyucchoi@naver.com>
2025-05-29 20:18:55 +0900

    20250529

--- level2/N개의_최소공배수/solution.py
+++ level2/N개의_최소공배수/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(arr):
     # 최대공약수를 구하는 함수 (유클리드 호제법)
     def gcd(a, b):
@@ -11,8 +12,22 @@ def solution(arr):
     
     # 배열의 모든 원소에 대해 순차적으로 LCM 계산
     result = arr[0]
-    
+
     for i in range(1, len(arr)):
         result = lcm(result, arr[i])
+    
+    return result
+
+# 방법 2
+import math
+
+def solution(arr):
+    def lcm(a, b):
+        return (a * b) // math.gcd(a, b)
+    
+    result = arr[0]
+    
+    for num in arr[1:]:
+        result = lcm(result, num)
     
     return result
