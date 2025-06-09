7df0100ec53abed89e352a60ac2989a729e065ec
kyucchoi <kyucchoi@naver.com>
2025-06-09 23:42:56 +0900

    20250609

--- level2/괄호_회전하기/solution.py
+++ level2/괄호_회전하기/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(s):
     answer = 0
     n = len(s)
@@ -22,5 +23,40 @@ def solution(s):
         else:
             if not stack:
                 answer += 1
-                
+
     return answer
+
+# 방법 2
+def solution(s):
+    # 조기 종료 조건들
+    if len(s) % 2 == 1:  # 홀수 길이는 불가능
+        return 0
+    
+    # 각 괄호 종류별 개수가 맞지 않으면 불가능
+    if (s.count('(') != s.count(')') or 
+        s.count('[') != s.count(']') or 
+        s.count('{') != s.count('}')):
+        return 0
+    
+    def is_valid_brackets(string):
+        stack = []
+        pairs = {'(': ')', '[': ']', '{': '}'}
+        
+        for char in string:
+            if char in pairs:
+                stack.append(char)
+            else:
+                if not stack or pairs[stack.pop()] != char:
+                    return False
+                
+        return len(stack) == 0
+    
+    count = 0
+
+    for x in range(len(s)):
+        rotated = s[x:] + s[:x]
+
+        if is_valid_brackets(rotated):
+            count += 1
+
+    return count
