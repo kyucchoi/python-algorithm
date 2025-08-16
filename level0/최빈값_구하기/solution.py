4fd33734ad6f615fc0ece2242153e9c953f71595
kyucchoi <kyucchoi@naver.com>
2025-08-16 20:29:59 +0900

    20250816

--- level0/최빈값_구하기/solution.py
+++ level0/최빈값_구하기/solution.py
@@ -1,16 +1,18 @@
-
 def solution(array):
-    # 각 숫자의 출현 횟수를 세기
-    count_dict = {}
-    
-    for num in array:
-        count_dict[num] = count_dict.get(num, 0) + 1
-    
-    # 최대 출현 횟수 찾기
-    max_count = max(count_dict.values())
-    
-    # 최대 출현 횟수를 가진 숫자들 찾기
-    modes = [num for num, count in count_dict.items() if count == max_count]
-    
-    # 최빈값이 여러 개면 -1, 하나면 그 값 반환
-    return -1 if len(modes) > 1 else modes[0]
+   frequency = {}
+
+   for num in array:
+       frequency[num] = frequency.get(num, 0) + 1
+   
+   max_count = max(frequency.values())
+   
+   mode_candidates = []
+   
+   for num, count in frequency.items():
+       if count == max_count:
+           mode_candidates.append(num)
+   
+   if len(mode_candidates) == 1:
+       return mode_candidates[0]
+   else:
+       return -1
