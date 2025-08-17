657a2d038442f3434c1ca8609404223709d25a27
kyucchoi <kyucchoi@naver.com>
2025-08-17 15:58:39 +0900

    20250817

--- level2/JadenCase_문자열_만들기/solution.py
+++ level2/JadenCase_문자열_만들기/solution.py
@@ -1,3 +1,21 @@
+# 방법 1
+def solution(s):
+    result = []
+    is_first_char = True  # 단어의 첫 문자인지 추적
+    
+    for char in s:
+        if char == ' ':
+            result.append(char)
+            is_first_char = True  # 다음 문자는 새 단어의 시작
+        elif is_first_char:
+            result.append(char.upper())  # 첫 문자는 대문자
+            is_first_char = False
+        else:
+            result.append(char.lower())  # 나머지는 소문자
+    
+    return ''.join(result)
+
+# 방법 2
 def solution(s):
     words = s.split(' ')  # 공백을 기준으로 단어 분리
     capitalized_words = []
