01233ecdae5eb87571678ea68d5aa737ddf5b3d0
kyucchoi <kyucchoi@naver.com>
2025-08-22 20:17:35 +0900

    20250822

--- level2/피보나치_수/solution.py
+++ level2/피보나치_수/solution.py
@@ -1,9 +1,7 @@
 def solution(n):
-    # 피보나치 수열의 처음 두 숫자
-    a, b = 0, 1
-    
-    # n번째 피보나치 수를 계산
-    for i in range(2, n + 1):
-        a, b = b, (a + b) % 1234567
-        
-    return b
+    prev, curr = 0, 1
+
+    for _ in range(2, n + 1):
+        prev, curr = curr, (prev + curr) % 1234567
+
+    return curr
