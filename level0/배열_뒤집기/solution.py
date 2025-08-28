703940dc4cd0430c99b332f27f66919547bc7c0d
kyucchoi <kyucchoi@naver.com>
2025-08-28 18:32:16 +0900

    20250828

---
+++ level0/배열_뒤집기/solution.py
@@ -0,0 +1,9 @@
+# 방법 1
+def solution(num_list):
+    num_list.reverse()
+    
+    return num_list
+
+# 방법 2
+def solution(num_list):
+    return num_list[::-1]
---
+++ level0/짝수의_합/solution.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    
+    for num in range(n + 1):
+        if num % 2 == 0:
+            answer += num
+            
+    return answer
