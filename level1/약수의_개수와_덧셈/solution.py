b90d7691299bff1b5b5d729f8082b3ac5f71a01e
kyucchoi <kyucchoi@naver.com>
2025-09-03 21:50:03 +0900

    20250903

--- level1/약수의_개수와_덧셈/solution.py
+++ level1/약수의_개수와_덧셈/solution.py
@@ -1,18 +1,18 @@
 def solution(left, right):
     result = 0
     
-    for num in range(left, right + 1):
-        # 약수의 개수 계산
+    for number in range(left, right + 1):
         divisor_count = 0
         
-        for i in range(1, num + 1):
-            if num % i == 0:
+        for divisor in range(1, int(number**0.5) + 1):
+            if divisor * divisor == number:
                 divisor_count += 1
-        
-        # 약수의 개수가 짝수면 더하고, 홀수면 뺌
+            elif number % divisor == 0:
+                divisor_count += 2
+                
         if divisor_count % 2 == 0:
-            result += num
+            result += number
         else:
-            result -= num
-    
+            result -= number
+            
     return result
