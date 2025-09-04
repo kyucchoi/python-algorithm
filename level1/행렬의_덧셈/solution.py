796b575e12264881d5c3c1ceebeebbe52a995ec9
kyucchoi <kyucchoi@naver.com>
2025-09-04 19:25:36 +0900

    20250904

--- level1/행렬의_덧셈/solution.py
+++ level1/행렬의_덧셈/solution.py
@@ -1,21 +1,25 @@
 # 방법 1
 def solution(arr1, arr2):
-    return [[arr1[i][j] + arr2[i][j] for j in range(len(arr1[0]))] for i in range(len(arr1))]
+    answer = []
 
-def solution(arr1, arr2):
-    result = []
-    
-    # 바깥쪽 반복문: 행(row)을 처리
     for i in range(len(arr1)):
-        row_result = []
-        
-        # 안쪽 반복문: 각 행의 열(column)을 처리
-        for j in range(len(arr1[0])):
-            # arr1과 arr2의 동일한 위치(i행 j열)의 원소를 더함
-            sum_value = arr1[i][j] + arr2[i][j]
-            row_result.append(sum_value)
-        
-        # 완성된 행을 결과 행렬에 추가
-        result.append(row_result)
+        row = []
     
-    return result
+        for j in range(len(arr1[0])):
+            row.append(arr1[i][j] + arr2[i][j])
+
+        answer.append(row)
+        
+    return answer
+
+# 방법 2
+def solution(arr1, arr2):
+    rows = len(arr1)
+    cols = len(arr1[0])
+    answer = [[0] * cols for _ in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+        
+    return answer
