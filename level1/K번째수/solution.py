48cdc2cd7e60b6d8d468f8c664ad267e28b74513
kyucchoi <kyucchoi@naver.com>
2025-09-23 18:51:13 +0900

    20250923

--- level1/K번째수/solution.py
+++ level1/K번째수/solution.py
@@ -3,11 +3,8 @@ def solution(array, commands):
     answer = []
     
     for i, j, k in commands:
-        # 배열 자르기 (i - 1부터 j까지)
         sliced = array[i - 1:j]
-        # 정렬
         sliced.sort()
-        # k번째 수 추가
         answer.append(sliced[k - 1])
         
     return answer
@@ -16,11 +13,9 @@ def solution(array, commands):
 def solution(array, commands):
     answer = []
     
-    for i, j, k in commands:  # commands의 각 요소를 직접 i, j, k로 언패킹
+    for i, j, k in commands:
         sliced = array[i - 1:j]
-
         sorted_arr = sorted(sliced)
-
         answer.append(sorted_arr[k - 1])
     
     return answer
