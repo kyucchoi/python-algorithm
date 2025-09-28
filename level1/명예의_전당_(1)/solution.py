c78ac05ffad2ab66ddc4b524ba6b70ca4bdbf933
kyucchoi <kyucchoi@naver.com>
2025-09-28 15:54:27 +0900

    20250928

--- level1/명예의_전당_(1)/solution.py
+++ level1/명예의_전당_(1)/solution.py
@@ -1,21 +1,17 @@
 # 방법 1
 def solution(k, score):
-    # 명예의 전당 목록
     hall_of_fame = []
-    # 매일 발표되는 최하위 점수
     answer = []
     
-    for s in score:
-        # 새 점수를 명예의 전당에 추가
-        hall_of_fame.append(s)
-        # 높은 점수 순으로 정렬
+    for daily_score in score:
+        hall_of_fame.append(daily_score)
         hall_of_fame.sort(reverse = True)
-        # 명예의 전당 크기를 k로 제한
+        
         if len(hall_of_fame) > k:
-            hall_of_fame = hall_of_fame[:k]
-        # 최하위 점수 추가
-        answer.append(hall_of_fame[-1])
-    
+            hall_of_fame.pop()
+            
+        answer.append(min(hall_of_fame))
+        
     return answer
 
 # 방법 2
