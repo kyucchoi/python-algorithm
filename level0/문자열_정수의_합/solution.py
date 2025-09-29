179734e75584280161be13a89caf1c947a2dcb69
kyucchoi <kyucchoi@naver.com>
2025-09-29 23:35:00 +0900

    20250929

--- level0/문자열_정수의_합/solution.py
+++ level0/문자열_정수의_합/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(num_str):
     answer = 0
     
@@ -5,3 +6,7 @@ def solution(num_str):
         answer += int(num)
         
     return answer
+
+# 방법 2
+def solution(num_str):
+    return sum(map(int, list(num_str)))
