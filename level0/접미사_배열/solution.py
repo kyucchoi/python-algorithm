cf28f14fc71b254b1942fa356079b7f27f4a8f8e
kyucchoi <kyucchoi@naver.com>
2025-10-05 00:39:35 +0900

    20251005

--- level0/접미사_배열/solution.py
+++ level0/접미사_배열/solution.py
@@ -1,11 +1,7 @@
 def solution(my_string):
     result = []
     
-    # 모든 가능한 접미사 만들기
     for i in range(len(my_string)):
         result.append(my_string[i:])
-        
-    # 사전순 정렬
-    result.sort()
     
-    return result
+    return sorted(result)
