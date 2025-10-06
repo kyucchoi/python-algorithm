fb58ff5351f0f7f503209656a77943be1dc420e6
kyucchoi <kyucchoi@naver.com>
2025-10-06 23:14:41 +0900

    20251006

--- level0/수_조작하기_2/solution.py
+++ level0/수_조작하기_2/solution.py
@@ -2,17 +2,15 @@ def solution(numLog):
     result = ''
     
     for i in range(1, len(numLog)):
-        # 현재 값과 이전 값의 차이를 계산
         diff = numLog[i] - numLog[i - 1]
         
-        # 차이에 따라 해당하는 문자 추가
         if diff == 1:
             result += 'w'
         elif diff == -1:
             result += 's'
         elif diff == 10:
             result += 'd'
-        else:  # diff == -10
+        elif diff == -10:
             result += 'a'
             
     return result
