6bc590449a081f1e6f0c8430cdf9fdbab69caa61
kyucchoi <kyucchoi@naver.com>
2025-10-09 23:02:17 +0900

    20251009

--- level0/n보다_커질_때까지_더하기/solution.py
+++ level0/n보다_커질_때까지_더하기/solution.py
@@ -1,10 +1,10 @@
 def solution(numbers, n):
-    sum = 0
+    answer = 0
     
     for num in numbers:
-        sum += num      # 현재 숫자를 더함
+        answer += num      # 현재 숫자를 더함
         
-        if sum > n:     # 합이 n보다 커지면
+        if answer > n:     # 합이 n보다 커지면
             break       # 반복문 종료
             
-    return sum
+    return answer
