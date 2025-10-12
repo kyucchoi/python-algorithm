9efb1bdfeba0be0ac6db3c2f8313cebfbd403bef
kyucchoi <kyucchoi@naver.com>
2025-10-12 09:39:21 +0900

    20251012

--- level0/리스트_자르기/solution.py
+++ level0/리스트_자르기/solution.py
@@ -2,10 +2,10 @@ def solution(n, slicer, num_list):
     a, b, c = slicer  # slicer의 세 값을 a, b, c로 분리
     
     if n == 1:
-        return num_list[:b + 1]     # 0부터 b까지
+        return num_list[:b + 1]
     elif n == 2:
-        return num_list[a:]       # a부터 끝까지
+        return num_list[a:]
     elif n == 3:
-        return num_list[a:b + 1]    # a부터 b까지
-    else:  # n == 4
-        return num_list[a:b + 1:c]  # a부터 b까지 c 간격으로
+        return num_list[a:b + 1]
+    elif n == 4:
+        return num_list[a:b + 1:c]
