82562760ba4f21fe914aed260eabf32a7966008b
kyucchoi <kyucchoi@naver.com>
2025-10-15 23:45:52 +0900

    20251015

--- level0/간단한_식_계산하기/solution.py
+++ level0/간단한_식_계산하기/solution.py
@@ -2,14 +2,12 @@ def solution(binomial):
     # 공백으로 분리하여 a, op, b 추출
     a, op, b = binomial.split()
     
-    # 문자열을 정수로 변환
     a = int(a)
     b = int(b)
     
-    # 연산자에 따라 계산
     if op == '+':
         return a + b
     elif op == '-':
         return a - b
-    else:
+    elif op == '*':
         return a * b
