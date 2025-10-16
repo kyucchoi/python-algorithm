d6dbe92121820aededd64fef05196f0e974543ae
kyucchoi <kyucchoi@naver.com>
2025-10-16 22:31:57 +0900

    20251016

--- level2/예상_대진표/solution.py
+++ level2/예상_대진표/solution.py
@@ -1,10 +1,26 @@
+# 방법 1
 def solution(n, a, b):
-    round_num = 1
-
-    while True:
-        if (a - 1) // 2 == (b - 1) // 2:  # 같은 대진인가?
-            return round_num
-        
-        a = (a + 1) // 2  # 다음 라운드 번호
+    round_count = 0
+    
+    while a != b:
+        # 다음 라운드 번호로 변경
+        a = (a + 1) // 2
         b = (b + 1) // 2
-        round_num += 1
+        round_count += 1
+    
+    return round_count
+
+# 방법 2
+def solution(n, a, b):
+    round_count = 0
+    
+    while True:
+        round_count += 1
+        
+        # 다음 라운드 번호 계산
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        
+        # 같은 번호 = 만남
+        if a == b:
+            return round_count
