a24b155eb8fc1c7cb7c33a409d96adb46d8616af
kyucchoi <kyucchoi@naver.com>
2025-10-18 11:59:17 +0900

    20251018

--- level0/길이에_따른_연산/solution.py
+++ level0/길이에_따른_연산/solution.py
@@ -2,7 +2,7 @@ def solution(num_list):
     if len(num_list) >= 11:
         return sum(num_list)
     else:
-        result = 1 # 곱셈의 시작값을 1로 설정 (0으로 하면 모든 결과가 0이 됨)
+        result = 1
 
         for num in num_list:
             result *= num
