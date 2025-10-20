ee156a1632fd74f50cdc2402208b0e3efc06e1f4
kyucchoi <kyucchoi@naver.com>
2025-10-20 19:37:57 +0900

    20251020

--- level0/공_던지기/solution.py
+++ level0/공_던지기/solution.py
@@ -1,10 +1,8 @@
 def solution(numbers, k):
-    # 현재 공을 가진 사람의 인덱스 (1번부터 시작하므로 인덱스 0)
     current_index = 0
     
     # k번째까지 공을 던지기
     for i in range(k):
-        # k번째에 도달하면 현재 사람이 답
         if i == k - 1:
             return numbers[current_index]
         
