c89cfb0854d3aef4d9504a1e1735cc28f9b2b446
kyucchoi <kyucchoi@naver.com>
2025-10-21 10:50:43 +0900

    20251021

--- level2/연속_부분_수열_합의_개수/solution.py
+++ level2/연속_부분_수열_합의_개수/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(elements):
     n = len(elements)
     # 원형을 위해 배열 확장
@@ -12,5 +13,21 @@ def solution(elements):
         for length in range(1, n + 1):
             current_sum += extended[start + length - 1]
             sums.add(current_sum)
+    
+    return len(sums)
+
+# 방법 2
+def solution(elements):
+    n = len(elements)
+    sums = set()
+    
+    for length in range(1, n + 1):
+        for start in range(n):
+            total = 0
+
+            for i in range(length):
+                total += elements[(start + i) % n]
+                
+            sums.add(total)
     
     return len(sums)
