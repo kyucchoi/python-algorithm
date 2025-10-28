df599be0885b8d3bf6817e874223d25dea99d257
kyucchoi <kyucchoi@naver.com>
2025-10-28 22:39:45 +0900

    20251028

--- level1/콜라_문제/solution.py
+++ level1/콜라_문제/solution.py
@@ -1,13 +1,9 @@
 def solution(a, b, n):
-    # 받은 총 콜라 수
     total_cola = 0
     
-    # 가지고 있는 빈 병이 a개 이상인 동안 반복
     while n >= a:
-        # 교환으로 받는 콜라 수
         received = (n // a) * b
         
-        # 총 콜라 수에 더함
         total_cola += received
         
         # 남은 빈 병 계산: 원래 있던 빈 병 - 교환한 빈 병 + 새로 받은 콜라
--- level1/콜라츠_추측/solution.py
+++ level1/콜라츠_추측/solution.py
@@ -3,23 +3,17 @@ def solution(num):
     if num == 1:
         return 0
     
-    # 작업 횟수를 세는 변수
     count = 0
     
-    # 1이 될 때까지 반복
     while num != 1:
-        # 500번 넘게 반복하면 -1 반환
         if count >= 500:
             return -1
         
-        # 짝수라면 2로 나누기
         if num % 2 == 0:
             num = num // 2
-        # 홀수라면 3을 곱하고 1 더하기
         else:
             num = num * 3 + 1
             
-        # 작업 횟수 증가
         count += 1
     
     return count
