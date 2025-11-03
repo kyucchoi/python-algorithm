ac6aac736236f0761fd4ba4fb46df26a1c83ea64
kyucchoi <kyucchoi@naver.com>
2025-11-03 23:25:06 +0900

    20251103

--- level2/2_x_n_타일링/solution.py
+++ level2/2_x_n_타일링/solution.py
@@ -1,14 +1,12 @@
 def solution(n):
     MOD = 1000000007
     
-    # 기본 케이스
     if n <= 2:
         return n
     
     # 이전 2개 값만 저장 (공간 최적화)
     prev1, prev2 = 1, 2  # dp[1], dp[2]
     
-    # 3부터 n까지 계산
     for i in range(3, n + 1):
         current = (prev1 + prev2) % MOD
         prev1, prev2 = prev2, current
--- level2/다음_큰_숫자/solution.py
+++ level2/다음_큰_숫자/solution.py
@@ -17,11 +17,10 @@ def solution(n):
     # n의 2진수에서 1의 개수
     one_count = bin(n).count('1')
     
-    # n + 1부터 검사
     next_num = n + 1
     
-    # 조건 만족하는 수 찾기
     while True:
         if bin(next_num).count('1') == one_count:
             return next_num
+        
         next_num += 1
