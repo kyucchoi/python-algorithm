e25636a422a311d57410f607f3112b3bb2b60313
kyucchoi <kyucchoi@naver.com>
2025-11-05 21:50:43 +0900

    20251105

--- level2/[1차]_캐시/solution.py
+++ level2/[1차]_캐시/solution.py
@@ -1,6 +1,5 @@
 # 방법 1
 def solution(cacheSize, cities):
-    # 캐시 크기가 0이면 모든 검색이 miss
     if cacheSize == 0:
         return len(cities) * 5
     
@@ -12,14 +11,15 @@ def solution(cacheSize, cities):
         
         if city in cache:
             # Cache Hit: 해당 도시를 최신으로 이동
-            cache.remove(city)  # 기존 위치에서 제거
-            cache.append(city)  # 맨 뒤에 추가 (최신으로 표시)
+            cache.remove(city)
+            cache.append(city)
             time += 1
         else:
             # Cache Miss: 새로운 도시 추가
             if len(cache) >= cacheSize:
-                cache.pop(0)  # 가장 오래된 것(맨 앞) 제거
-            cache.append(city)  # 새 도시 추가
+                cache.pop(0)
+
+            cache.append(city)
             time += 5
     
     return time
