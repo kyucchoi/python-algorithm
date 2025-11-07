3b9623867fd6850c92b7b30b508fdbc1f269d3b1
kyucchoi <kyucchoi@naver.com>
2025-11-07 12:38:17 +0900

    20251107

--- level2/전화번호_목록/solution.py
+++ level2/전화번호_목록/solution.py
@@ -1,7 +1,6 @@
 def solution(phone_book):
-    phone_book.sort()  # 전화번호 정렬
-    
-    # 이웃한 번호끼리만 비교
+    phone_book.sort()
+
     for i in range(len(phone_book) - 1):
         if phone_book[i + 1].startswith(phone_book[i]):
             return False
