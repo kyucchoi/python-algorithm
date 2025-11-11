226159e6cc4d246e8d693ac8df4f698eb1997a32
kyucchoi <kyucchoi@naver.com>
2025-11-11 11:52:34 +0900

    20251111

--- level1/두_정수_사이의_합/solution.py
+++ level1/두_정수_사이의_합/solution.py
@@ -1,9 +1,7 @@
 def solution(a, b):
-    # a와 b의 대소관계에 따라 시작점과 끝점을 정합니다
     start = min(a, b)
     end = max(a, b)
     
-    # start부터 end까지의 모든 정수를 더합니다
     total = 0
     
     for i in range(start, end + 1):
