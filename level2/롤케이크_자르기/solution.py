eba3465b5256cb3899dc8d53f0249a9ce0fcd0da
kyucchoi <kyucchoi@naver.com>
2025-11-12 23:07:42 +0900

    20251112

--- level2/롤케이크_자르기/solution.py
+++ level2/롤케이크_자르기/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(topping):
     n = len(topping)
     
@@ -28,3 +29,31 @@ def solution(topping):
             count += 1
     
     return count
+
+# 방법 2
+def solution(topping):
+    answer = 0
+    
+    # 왼쪽 조각의 토핑 종류를 저장할 set
+    left_set = set()
+    # 오른쪽 조각의 토핑 개수를 저장할 dict
+    right_dict = {}
+    
+    # 처음에 모든 토핑을 오른쪽에 배치
+    for t in topping:
+        right_dict[t] = right_dict.get(t, 0) + 1
+    
+    for i in range(len(topping) - 1):
+        current = topping[i]
+        
+        left_set.add(current)
+        
+        right_dict[current] -= 1
+
+        if right_dict[current] == 0:
+            del right_dict[current]
+        
+        if len(left_set) == len(right_dict):
+            answer += 1
+    
+    return answer
