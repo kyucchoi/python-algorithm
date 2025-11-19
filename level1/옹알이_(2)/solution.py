9ffc9b6c0a2aae270ce078651700cdefc3b9d28a
kyucchoi <kyucchoi@naver.com>
2025-11-19 19:10:04 +0900

    20251119

--- level1/옹알이_(2)/solution.py
+++ level1/옹알이_(2)/solution.py
@@ -3,16 +3,22 @@ def solution(babbling):
     pronounceable = ['aya', 'ye', 'woo', 'ma']
     
     for word in babbling:
+        has_consecutive = False
+        
         for p in pronounceable:
-            if p * 2 in word:  # "aya" * 2 = "ayaaya"
+            if p * 2 in word:
+                has_consecutive = True
                 break
-        else:
-            temp_word = word
-
-            for p in pronounceable:
-                temp_word = temp_word.replace(p, ' ')
-            
-            if temp_word.strip() == '':
-                count += 1
+        
+        if has_consecutive:
+            continue
+        
+        temp_word = word
+        
+        for p in pronounceable:
+            temp_word = temp_word.replace(p, ' ')
+        
+        if temp_word.strip() == '':
+            count += 1
     
     return count
