c9ca300adb22fcf71f7315eb562b9a1fb6b18acd
kyucchoi <kyucchoi@naver.com>
2025-11-20 22:24:37 +0900

    20251120

--- level2/모음사전/solution.py
+++ level2/모음사전/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 from itertools import product
 
 def solution(word):
@@ -11,3 +12,14 @@ def solution(word):
     words.sort()
     
     return words.index(word) + 1
+
+# 방법 2
+def solution(word):
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    weights = [781, 156, 31, 6, 1]
+    answer = 0
+    
+    for i, char in enumerate(word):
+        answer += vowels.index(char) * weights[i] + 1
+    
+    return answer
