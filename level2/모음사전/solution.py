53d51b27b15e76719eafc401a71f61d9117e3e26
kyucchoi <kyucchoi@naver.com>
2025-11-21 21:00:55 +0900

    20251121

--- level2/모음사전/solution.py
+++ level2/모음사전/solution.py
@@ -1,17 +1,20 @@
 # 방법 1
-from itertools import product
-
 def solution(word):
-    words = []
     vowels = ['A', 'E', 'I', 'O', 'U']
+    answer = 0
     
-    for length in range(1, 6):
-        for combo in product(vowels, repeat = length):
-            words.append(''.join(combo))
+    # 각 자리에서 한 모음으로 시작하는 단어 개수
+    # weights[i] = 5^4 + 5^3 + 5^2 + 5^1 + 5^0 (i번째 이후 자리)
+    weights = [781, 156, 31, 6, 1]
     
-    words.sort()
+    for i in range(len(word)):
+        char = word[i]
+        
+        char_idx = vowels.index(char)
+        
+        answer += char_idx * weights[i] + 1
     
-    return words.index(word) + 1
+    return answer
 
 # 방법 2
 def solution(word):
