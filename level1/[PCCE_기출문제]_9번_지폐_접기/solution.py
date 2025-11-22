59c3344759571c77b6038bef005b66d9ec07e48e
kyucchoi <kyucchoi@naver.com>
2025-11-22 19:08:00 +0900

    20251122

--- level1/[PCCE_기출문제]_9번_지폐_접기/solution.py
+++ level1/[PCCE_기출문제]_9번_지폐_접기/solution.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(wallet, bill):
     answer = 0
     
@@ -9,14 +10,36 @@ def solution(wallet, bill):
     while bill[0] > wallet[0] or bill[1] > wallet[1]:
         # bill[0]이 bill[1]보다 크다면 (큰 쪽을 접음)
         if bill[0] > bill[1]:
-            bill[0] = bill[0] // 2  # 2로 나누고 나머지 버림
-        else:
-            bill[1] = bill[1] // 2  # 2로 나누고 나머지 버림
+            bill[0] = bill[0] // 2
+        elif bill[0] < bill[1]:
+            bill[1] = bill[1] // 2
         
-        # 접었으므로 횟수 증가
         answer += 1
         
         # 지폐를 다시 작은 값, 큰 값 순으로 정렬 (90도 회전 가능성 고려)
         bill.sort()
+    
+    return answer
+
+# 방법 2
+def solution(wallet, bill):
+    answer = 0
+    
+    while True:
+        wallet_min = min(wallet)
+        wallet_max = max(wallet)
+        bill_min = min(bill)
+        bill_max = max(bill)
+        
+        # 들어가는지 체크 (그대로 또는 90도 회전)
+        if bill_min <= wallet_min and bill_max <= wallet_max:
+            break
+        
+        if bill[0] > bill[1]:
+            bill[0] //= 2
+        elif bill[0] < bill[1]:
+            bill[1] //= 2
+        
+        answer += 1
     
     return answer
