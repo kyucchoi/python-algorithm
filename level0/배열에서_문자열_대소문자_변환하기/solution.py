4a2ccbc6965d46514b8dd04582f63940b2f11623
kyucchoi <kyucchoi@naver.com>
2025-11-28 12:36:03 +0900

    20251128

--- level0/배열에서_문자열_대소문자_변환하기/solution.py
+++ level0/배열에서_문자열_대소문자_변환하기/solution.py
@@ -2,9 +2,9 @@ def solution(strArr):
     result = []
     
     for i, s in enumerate(strArr):
-        if i % 2 == 0:  # 짝수 인덱스
+        if i % 2 == 0:
             result.append(s.lower())
-        else:           # 홀수 인덱스
+        else:
             result.append(s.upper())
             
     return result
