41e8cb6d2acc2b48e844ef2614fc2be084e5959c
kyucchoi <kyucchoi@naver.com>
2025-11-29 20:23:32 +0900

    20251129

--- level0/배열의 길이를_2의_거듭제곱으로_만들기/solution.py
+++ level0/배열의 길이를_2의_거듭제곱으로_만들기/solution.py
@@ -1,12 +1,9 @@
 def solution(arr):
-    # 현재 배열의 길이
     length = len(arr)
     
-    # 2의 거듭제곱 중 length보다 크거나 같은 최소값 찾기
     target_length = 1
     
     while target_length < length:
         target_length *= 2
     
-    # 필요한 만큼 0 추가
     return arr + [0] * (target_length - length)
