fd3a3a012e61b8f0e267ac5b317ec00678718011
kyucchoi <kyucchoi@naver.com>
2025-12-02 17:22:55 +0900

    20251202

--- level2/뒤에_있는_큰_수_찾기/solution.py
+++ level2/뒤에_있는_큰_수_찾기/solution.py
@@ -2,11 +2,12 @@ def solution(numbers):
     n = len(numbers)
     answer = [-1] * n
     stack = []
-
+    
     for i in range(n):
+        # 현재 숫자가 스택에 있는 숫자들의 뒷 큰수인지 확인
         while stack and numbers[stack[-1]] < numbers[i]:
-            # 스택에서 인덱스를 꺼내어 해당 위치의 뒷 큰수를 현재 숫자로 설정
-            answer[stack.pop()] = numbers[i]
+            idx = stack.pop()
+            answer[idx] = numbers[i]
         
         stack.append(i)
     
