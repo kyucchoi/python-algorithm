11a3d974e525667e197ae4f59670443603b71855
kyucchoi <kyucchoi@naver.com>
2025-12-06 19:40:05 +0900

    20251206

--- level1/짝수와_홀수/solution.py
+++ level1/짝수와_홀수/solution.py
@@ -1,5 +1,5 @@
 def solution(num):
-    if num % 2 == 0:    # 2로 나눈 나머지가 0이면 짝수
+    if num % 2 == 0:
         return 'Even'
-    else:               # 2로 나눈 나머지가 1이면 홀수
+    else: 
         return 'Odd'
