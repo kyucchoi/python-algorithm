7951c7f8218c8e7e52bc0f50ec9d90232c9594ec
kyucchoi <kyucchoi@naver.com>
2025-12-14 12:33:50 +0900

    20251214

--- level0/수열과_구간_쿼리_2/solution.py
+++ level0/수열과_구간_쿼리_2/solution.py
@@ -1,10 +1,9 @@
 def solution(arr, queries):
-    result = []  # 결과를 저장할 리스트
+    result = []
     
     for s, e, k in queries:
-        min_value = float('inf')  # 최솟값을 찾기 위한 초기값
+        min_value = float('inf')
         
-        # s부터 e까지 범위 확인
         for i in range(s, e + 1):
             if arr[i] > k and arr[i] < min_value:
                 min_value = arr[i]
