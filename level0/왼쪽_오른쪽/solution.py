14c53f92804407bd775d1fe084bfe3b639a46d7d
kyucchoi <kyucchoi@naver.com>
2025-12-18 17:00:00 +0900

    20251218

--- level0/왼쪽_오른쪽/solution.py
+++ level0/왼쪽_오른쪽/solution.py
@@ -1,9 +1,8 @@
 def solution(str_list):
-    # l과 r의 인덱스 찾기
     for i in range(len(str_list)):
         if str_list[i] == 'l':
             return str_list[:i]  # l 왼쪽 부분 반환
         elif str_list[i] == 'r':
             return str_list[i + 1:]  # r 오른쪽 부분 반환
             
-    return []  # l이나 r이 없는 경우
+    return []
