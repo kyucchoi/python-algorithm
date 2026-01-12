73cd9e83c3e424b3f171327d6da41adedc40a109
kyucchoi <kyucchoi@naver.com>
2026-01-12 22:39:39 +0900

    20260112

--- level1/[1차]_비밀지도/solution.py
+++ level1/[1차]_비밀지도/solution.py
@@ -1,15 +1,13 @@
 def solution(n, arr1, arr2):
-    result = []
+    answer = []
     
     for i in range(n):
-        # 두 배열의 해당 위치 값을 이진수로 변환 후 OR 연산
-        binary = bin(arr1[i] | arr2[i])[2:]  # bin() 결과는 '0b'로 시작하므로 [2:]로 제거
+        num = arr1[i] | arr2[i]
         
-        # n 길이에 맞게 앞에 0 채우기
-        binary = binary.zfill(n)
+        binary = bin(num)[2:].zfill(n)
         
         row = binary.replace('1', '#').replace('0', ' ')
         
-        result.append(row)
+        answer.append(row)
     
-    return result
+    return answer
