562149fab0a67a3e9493b9880c0ce35409091594
kyucchoi <kyucchoi@naver.com>
2026-01-13 22:40:21 +0900

    20260113

--- level2/[1차]_캐시/solution.py
+++ level2/[1차]_캐시/solution.py
@@ -7,18 +7,15 @@ def solution(cacheSize, cities):
     time = 0
     
     for city in cities:
-        city = city.lower()  # 대소문자 구분 안 함
+        city = city.lower()
         
         if city in cache:
-            # Cache Hit: 해당 도시를 최신으로 이동
             cache.remove(city)
             cache.append(city)
             time += 1
         else:
-            # Cache Miss: 새로운 도시 추가
             if len(cache) >= cacheSize:
                 cache.pop(0)
-
             cache.append(city)
             time += 5
     
@@ -28,18 +25,22 @@ def solution(cacheSize, cities):
 from collections import deque
 
 def solution(cacheSize, cities):
-    cache = deque(maxlen = cacheSize)
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    cache = deque(maxlen=cacheSize)
     time = 0
-
+    
     for city in cities:
         city = city.lower()
-
+        
         if city in cache:
             cache.remove(city)
             cache.append(city)
             time += 1
+        
         else:
             cache.append(city)
             time += 5
-            
+    
     return time
