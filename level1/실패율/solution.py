c5e83dcdeccf23039a06d61f25c385622db9dc7d
kyucchoi <kyucchoi@naver.com>
2026-01-22 12:42:17 +0900

    20260122

--- level1/실패율/solution.py
+++ level1/실패율/solution.py
@@ -1,14 +1,13 @@
+from collections import Counter
+
 def solution(N, stages):
-    # 1. 각 스테이지별 인원 수 세기
-    from collections import Counter
     stage_count = Counter(stages)
     
-    # 2. 실패율 계산
     total_players = len(stages)
     failure_rates = []
     
     for stage in range(1, N + 1):
-        if total_players == 0:  # 도달한 사람이 없으면
+        if total_players == 0:
             failure_rate = 0
         else:
             stuck = stage_count.get(stage, 0)
@@ -17,8 +16,6 @@ def solution(N, stages):
         
         failure_rates.append((failure_rate, stage))
     
-    # 3. 정렬 (실패율 내림차순, 같으면 스테이지 오름차순)
     failure_rates.sort(key=lambda x: (-x[0], x[1]))
     
-    # 4. 스테이지 번호만 추출
     return [stage for _, stage in failure_rates]
