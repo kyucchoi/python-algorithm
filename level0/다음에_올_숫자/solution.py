0336f69dcbe938b16d39faf55d8650a9952621f6
kyucchoi <kyucchoi@naver.com>
2026-01-23 16:46:06 +0900

    20260123

--- level0/다음에_올_숫자/solution.py
+++ level0/다음에_올_숫자/solution.py
@@ -1,12 +1,9 @@
 def solution(common):
-    # 1. 등차수열인지 확인
     diff1 = common[1] - common[0]
     diff2 = common[2] - common[1]
 
-    # 2. 등차수열인 경우
     if diff1 == diff2:
         return common[-1] + diff1
-    # 3. 등비수열인 경우
     else:
         ratio = common[1] // common[0]
 
