f0b7f9f6cb8867d083e0db86be753b3c2818c4d7
kyucchoi <kyucchoi@naver.com>
2026-01-25 23:31:36 +0900

    20260125

--- level0/할_일_목록/solution.py
+++ level0/할_일_목록/solution.py
@@ -2,7 +2,7 @@ def solution(todo_list, finished):
     result = []
     
     for i in range(len(todo_list)):
-        if not finished[i]:    # finished[i]가 False인 경우
+        if not finished[i]:
             result.append(todo_list[i])
             
     return result
