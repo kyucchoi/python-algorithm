745841f44445a13418a8a3a2fef8cf54e6b5f781
kyucchoi <kyucchoi@naver.com>
2026-01-28 20:56:58 +0900

    20260128

--- level1/같은_숫자는_싫어/solution.py
+++ level1/같은_숫자는_싫어/solution.py
@@ -1,9 +1,8 @@
 # 방법 1
 def solution(arr):
-    result = []  # 결과를 저장할 리스트
+    result = []
     
     for num in arr:
-        # result가 비어있거나, 마지막 숫자와 현재 숫자가 다르면
         if not result or result[-1] != num:
             result.append(num)
             
