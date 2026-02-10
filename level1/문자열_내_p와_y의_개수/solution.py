e96ef907e4206ac94cf29da743b9bb42a1315c43
kyucchoi <kyucchoi@naver.com>
2026-02-10 20:27:38 +0900

    20260210

--- level1/문자열_내_p와_y의_개수/solution.py
+++ level1/문자열_내_p와_y의_개수/solution.py
@@ -1,10 +1,7 @@
 def solution(s):
-    # 모든 문자를 소문자로 변환
     s = s.lower()
     
-    # 'p'와 'y'의 개수를 세기
     p_count = s.count('p')
     y_count = s.count('y')
     
-    # 개수 비교
     return p_count == y_count
