a474880a8b737514be19a77812d17160477d19fa
kyucchoi <kyucchoi@naver.com>
2026-02-15 23:27:10 +0900

    20260215

--- level1/삼총사/solution.py
+++ level1/삼총사/solution.py
@@ -3,7 +3,6 @@ def solution(number):
     count = 0
     n = len(number)
     
-    # 3중 반복문으로 모든 조합 확인
     for i in range(n):
         for j in range(i + 1, n):
             for k in range(j + 1, n):
@@ -18,7 +17,6 @@ from itertools import combinations
 def solution(number):
     count = 0
     
-    # 모든 3개 숫자 조합 생성
     for combo in combinations(number, 3):
         if sum(combo) == 0:
             count += 1
