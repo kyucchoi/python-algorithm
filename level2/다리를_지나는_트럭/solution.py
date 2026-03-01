10861545396fd23892e3d039a24cfcf63ac625ee
kyucchoi <kyucchoi@naver.com>
2026-03-01 23:55:25 +0900

    20260301

--- level2/다리를_지나는_트럭/solution.py
+++ level2/다리를_지나는_트럭/solution.py
@@ -2,18 +2,16 @@ from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
     time = 0
-    bridge = deque([0] * bridge_length)  # 다리 초기화
-    trucks = deque(truck_weights)  # 대기 트럭
-    current_weight = 0  # 다리 위 무게
+    bridge = deque([0] * bridge_length)
+    trucks = deque(truck_weights)
+    current_weight = 0
     
     while trucks or current_weight > 0:
         time += 1
         
-        # 1. 다리 맨 앞 트럭 내보내기
         out = bridge.popleft()
         current_weight -= out
         
-        # 2. 새 트럭 올리기
         if trucks:
             if current_weight + trucks[0] <= weight:
                 truck = trucks.popleft()
